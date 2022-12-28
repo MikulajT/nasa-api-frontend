934e5edbc12f30d9ec1595e3b47197c5dd9ab530
Tomas Mikulaj <mikulajt@gmail.com>
2022-12-28 17:46:24 +0100

    Add page for APOD

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-function App() {
-  return (
-    <div className="App">
+import { useState, useEffect } from 'react';
 
+type Apod = {
+  hdurl?: string;
+  date?: string;
+  copyright?: string;
+  explanation?: string;
+  title?: string;
+};
+
+function App() {
+  const [apod, setApod] = useState<Apod>({});
+  useEffect(() => {
+    fetch('http://localhost:5076/api/apod')
+       .then((response) => response.json())
+       .then((data) => {
+          console.log(data);
+          setApod(data);
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+ }, []);
+
+  return (
+    <div className="container-fluid">
+      <h1>Astronomy Picture of the Day</h1>
+      <h2>{apod.title} ({apod.date})</h2>
+      <div>
+        <img src={apod.hdurl} alt={apod.title}></img>
+      </div>
+      <p>{apod.explanation}</p>
+      <p>{apod.copyright}</p>
     </div>
   );
 }
