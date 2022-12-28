39caae88019e4c0edb9e6778a0399d2769a65331
Tomas Mikulaj <mikulajt@gmail.com>
2022-12-28 21:47:20 +0100

    Add basic css with Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,35 @@ type Apod = {
 
 function App() {
   const [apod, setApod] = useState<Apod>({});
+  const [isApodLoaded, setIsApodLoaded] = useState<boolean>(false);
   useEffect(() => {
     fetch('http://localhost:5076/api/apod')
        .then((response) => response.json())
        .then((data) => {
-          console.log(data);
           setApod(data);
+          setIsApodLoaded(true);
        })
        .catch((err) => {
           console.log(err.message);
        });
  }, []);
 
-  return (
-    <div className="container-fluid">
-      <h1>Astronomy Picture of the Day</h1>
-      <h2>{apod.title} ({apod.date})</h2>
-      <div>
-        <img src={apod.hdurl} alt={apod.title}></img>
+  if (isApodLoaded) {
+    return (
+      <div className='container-fluid'>
+      <h1 className='text-center'>Astronomy Picture of the Day</h1>
+      <h2 className='text-center'>{apod.title} ({apod.date})</h2>
+      <img src={apod.hdurl} className='d-block m-auto' id='apod' alt={apod.title}></img>
+      <p id='explanation'>Description: {apod.explanation}</p>
+      <p className='text-center'>Image copyright: {apod.copyright}</p>
       </div>
-      <p>{apod.explanation}</p>
-      <p>{apod.copyright}</p>
-    </div>
-  );
+    );
+  }
+  else {
+    return (
+      <h1 className='text-center'>Loading APOD...</h1>
+    );
+  }
 }
 
 export default App;
