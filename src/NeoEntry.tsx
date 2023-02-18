e779556e6eb763e918e846af2b5e405905844acc
Tomas Mikulaj <mikulajt@gmail.com>
2023-02-18 12:36:01 +0100

    Add left border to NEOs entries

--- src/NeoEntry.tsx
+++ src/NeoEntry.tsx
@@ -1,4 +1,4 @@
-import { Typography, Box, Tooltip } from '@mui/material';
+import { Typography, Box, Tooltip, Link } from '@mui/material';
 
 interface ChildProps {
     index: number,
@@ -12,15 +12,25 @@ interface ChildProps {
  }
 
 const NeoEntry: React.FC<ChildProps> = (props) => {
+
+    function getNeoLink(neoName: string) : string {
+        const neoNameWithoutCommas = neoName.slice(1,-1);
+        const encodedNeoName = encodeURIComponent(neoNameWithoutCommas);
+        return `https://ssd.jpl.nasa.gov/tools/sbdb_lookup.html#/?sstr=${encodedNeoName}&view=VOP`;
+    }
+
     return (
         <Box 
-            className="box-border"
-            sx={{ border: 1, borderColor: 'text.primary', margin: 1, p: 1 }} 
+            className={`box-border ${props.isHazardous ? "left-pink-corner" : "left-blue-corner"}`}
+            sx={{ border: 1, borderColor: 'text.primary', m: 1, p: 1 }} 
             onMouseEnter={() => props.showTooltip(props.index, props.isHazardous)} 
             onMouseLeave={() => props.hideTooltip()}
         >
             <Typography variant="body1">
-                Name: {props.name}
+                Name:&nbsp;
+                <Link href={getNeoLink(props.name)}>
+                    {props.name}
+                </Link>
             </Typography>
             <Typography variant="body1">
                 Date: {props.date}
