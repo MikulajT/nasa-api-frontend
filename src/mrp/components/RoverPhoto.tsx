cf7c81712a4400f298f1dfc949373aea749111cd
Tomas Mikulaj <mikulajt@gmail.com>
2023-05-08 11:19:09 +0200

    Modify mars rover photos image list

--- src/mrp/components/RoverPhoto.tsx
+++ src/mrp/components/RoverPhoto.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import Zoom from 'react-medium-image-zoom'
 import { v4 as uuidv4 } from 'uuid';
 import { IRoverPhotoProps } from "../interfaces/IRoverPhotoProps";
@@ -6,12 +7,14 @@ import { IRoverPhotoProps } from "../interfaces/IRoverPhotoProps";
 const RoverPhoto : React.FC<IRoverPhotoProps> = (props) => {
   return (
     <Zoom>
-      <img
-        key={uuidv4()}
-        src={props.imgSrc}
-        alt={props.cameraName}
-        loading="lazy"
-      />
+      <Box>
+        <img
+          key={uuidv4()}
+          src={props.imgSrc}
+          alt={props.cameraName}
+          loading="lazy"
+        />
+      </Box>
     </Zoom>
   );
 }
